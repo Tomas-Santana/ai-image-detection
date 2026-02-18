875613487ad833825ea2b61b325e9bc5fe040c4e
Tomas Santana <tomas.30604530@uru.edu>
2026-02-18 12:04:47 -0400

    chore: delete unused code

--- data/dataloader.py
+++ data/dataloader.py
@@ -1,6 +1,5 @@
 from torch.utils.data import Dataset, DataLoader
 import torch
-from torchvision import transforms
 from torchvision.transforms import v2
 import os
 from options.data_options import DatasetOptions
@@ -26,11 +25,10 @@ class Processor:
         opt: DatasetOptions,
         *,
         train: bool,
-        input_size: int = 256,
+        input_size: int = 512,
         crop_size: int = 224,
     ):
         self._train = train
-        self._input_size = input_size
         self._crop_size = crop_size
 
         self._to_image = v2.ToImage()
@@ -50,6 +48,13 @@ class Processor:
                 ),
             ]
         )
+        
+        self._make_square = v2.Compose(
+            [
+                v2.Resize(input_size, antialias=True),
+                v2.CenterCrop(input_size),
+            ]
+        )
 
         self._make_cropped = v2.Compose(
             [
@@ -65,18 +70,37 @@ class Processor:
         self, pil_img: Image.Image
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         img = self._to_image(pil_img)
-        _, h, w = img.shape
         
         if self._train:
             img = self._augment(img)
             
-        input_img = self._norm(self._to_float(img))
+        base = self._make_square(img)
+        _, h, w = base.shape
+            
+        input_img = self._norm(self._to_float(base))
 
-        cropped = self._make_cropped(img)
+        cropped = self._make_cropped(base)
         cropped_img = self._norm(self._to_float(cropped))
 
         scale = torch.tensor([h, w])
         return input_img, cropped_img, scale
+
+
+def patch_collate_train(batch):
+    input_img = torch.stack([item[0] for item in batch], dim=0)
+    cropped_img = torch.stack([item[1] for item in batch], dim=0)
+    target = torch.tensor([item[2] for item in batch])
+    scale = torch.stack([item[3] for item in batch], dim=0)
+    return [input_img, cropped_img, target, scale]
+
+
+def patch_collate_test(batch):
+    input_img = [item[0] for item in batch]
+    cropped_img = torch.stack([item[1] for item in batch], dim=0)
+    target = torch.tensor([item[2] for item in batch])
+    scale = torch.stack([item[3] for item in batch], dim=0)
+    filename = [item[4] for item in batch]
+    return [input_img, cropped_img, target, scale, filename]
 
 class GenImageDataset(Dataset):
     """Like GenImageDataset, but returns the old GLFF tuple.
@@ -91,7 +115,7 @@ class GenImageDataset(Dataset):
         opt: DatasetOptions,
         *,
         train: bool = True,
-        input_size: int = 256,
+        input_size: int = 512,
         crop_size: int = 224,
     ):
         super().__init__()
@@ -211,171 +235,7 @@ def get_loader(
         dataset,
         batch_size=opt.batch_size,
         shuffle=train,
+        collate_fn=patch_collate_train if train else patch_collate_test,
         num_workers=opt.workers,
         pin_memory=True,
     )
-
-
-"""
-=================
-CODE FROM THE ORIGINAL SSP
-Probably not needed
-=================
-"""
-
-"""mp = {
-    0: "imagenet_ai_0508_adm",
-    1: "imagenet_ai_0419_biggan",
-    2: "imagenet_glide",
-    3: "imagenet_midjourney",
-    4: "imagenet_ai_0424_sdv5",
-    5: "imagenet_ai_0419_vqdm",
-    6: "imagenet_ai_0424_wukong",
-}
-
-
-def sample_continuous(s):
-    if len(s) == 1:
-        return s[0]
-    if len(s) == 2:
-        rg = s[1] - s[0]
-        return random() * rg + s[0]
-    raise ValueError("Length of iterable s should be 1 or 2.")
-
-
-def sample_discrete(s):
-    if len(s) == 1:
-        return s[0]
-    return choice(s)
-
-
-def sample_randint(s):
-    if len(s) == 1:
-        return s[0]
-    return rd.randint(s[0], s[1])
-
-
-def gaussian_blur_gray(img, sigma):
-    if len(img.shape) == 3:
-        img_blur = np.zeros_like(img)
-        for i in range(img.shape[2]):
-            img_blur[:, :, i] = gaussian_filter(img[:, :, i], sigma=sigma)
-    else:
-        img_blur = gaussian_filter(img, sigma=sigma)
-    return img_blur
-
-
-def gaussian_blur(img, sigma):
-    gaussian_filter(img[:, :, 0], output=img[:, :, 0], sigma=sigma)
-    gaussian_filter(img[:, :, 1], output=img[:, :, 1], sigma=sigma)
-    gaussian_filter(img[:, :, 2], output=img[:, :, 2], sigma=sigma)
-
-
-def cv2_jpg(img, compress_val):
-    img_cv2 = img[:, :, ::-1]
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), compress_val]
-    result, encimg = cv2.imencode(".jpg", img_cv2, encode_param)
-    decimg = cv2.imdecode(encimg, 1)
-    return decimg[:, :, ::-1]
-
-
-def pil_jpg(img, compress_val):
-    out = BytesIO()
-    img = Image.fromarray(img)
-    img.save(out, format="jpeg", quality=compress_val)
-    img = Image.open(out)
-    # load from memory before ByteIO closes
-    img = np.array(img)
-    out.close()
-    return img
-
-
-jpeg_dict = {"cv2": cv2_jpg, "pil": pil_jpg}
-
-
-def jpeg_from_key(img, compress_val, key):
-    method = jpeg_dict[key]
-    return method(img, compress_val)
-
-
-def data_augment(img, opt: DatasetOptions):
-    img = np.array(img)
-
-    if random() < opt.transforms.get("blur", 0):
-        sig = sample_continuous(opt.blur_sig)
-        gaussian_blur(img, sig)
-
-    if random() < opt.transforms.get("jpeg", 0):
-        method = sample_discrete(opt.jpg_method)
-        qual = sample_randint(opt.jpg_qual)
-        img = jpeg_from_key(img, qual, method)
-
-    return Image.fromarray(img)
-
-
-class GenImageValDataset(Dataset):
-    def __init__(self, image_root, image_dir, is_real, opt):
-        super().__init__()
-        self.opt = opt
-        self.root = os.path.join(image_root, image_dir, "val")
-        if is_real:
-            self.img_path = os.path.join(self.root, "nature")
-            self.img_list = [
-                os.path.join(self.img_path, f) for f in os.listdir(self.img_path)
-            ]
-            self.img_len = len(self.img_list)
-            self.labels = torch.ones(self.img_len)
-        else:
-            self.img_path = os.path.join(self.root, "ai")
-            self.img_list = [
-                os.path.join(self.img_path, f) for f in os.listdir(self.img_path)
-            ]
-            self.img_len = len(self.img_list)
-            self.labels = torch.zeros(self.img_len)
-
-    def rgb_loader(self, path) -> Image.Image:
-        with open(path, "rb") as f:
-            img = Image.open(f)
-            return img.convert("RGB")
-
-    def __getitem__(self, index):
-        image = self.rgb_loader(self.img_list[index])
-        label = self.labels[index]
-        # image = processing(image, self.opt)
-        return image, label
-
-    def __len__(self):
-        return self.img_len
-
-
-def get_single_loader(opt, image_dir, is_real):
-    val_dataset = GenImageValDataset(
-        opt.image_root, image_dir=image_dir, is_real=is_real, opt=opt
-    )
-    val_loader = DataLoader(
-        val_dataset,
-        batch_size=opt.val_batchsize,
-        shuffle=False,
-        num_workers=4,
-        pin_memory=True,
-    )
-    return val_loader, len(val_dataset)
-
-
-def get_val_loader(opt):
-    choices = opt.choices
-    loader = []
-    for i, choice in enumerate(choices):
-        datainfo = dict()
-        if choice == 0 or choice == 1:
-            print("val on:", mp[i])
-            datainfo["name"] = mp[i]
-            datainfo["val_ai_loader"], datainfo["ai_size"] = get_single_loader(
-                opt, datainfo["name"], is_real=False
-            )
-            datainfo["val_nature_loader"], datainfo["nature_size"] = get_single_loader(
-                opt, datainfo["name"], is_real=True
-            )
-            loader.append(datainfo)
-    return loader
-"""
