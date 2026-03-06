372efe4e4e5f1d1f5416ea1dafe4c0ae97d35787
Tomas Santana <tomas.30604530@uru.edu>
2026-03-06 09:57:24 -0400

    feat: add unfreeze_last_layer option and update forward method to return additional features

--- networks/clip.py
+++ networks/clip.py
@@ -7,9 +7,8 @@ from typing import Tuple
 class CLIPResNet(nn.Module):
     visual: open_clip.model.ModifiedResNet
 
-    def __init__(self, model_name="RN50", frozen=True):
+    def __init__(self, model_name="RN50", frozen=True, unfreeze_last_layer=False):
         super(CLIPResNet, self).__init__()
-        # Load CLIP model (jit=False to allow hooking/getting attributes)
         self.clip_model, _, _ = open_clip.create_model_and_transforms(
             model_name, pretrained="openai", jit=False
         )
@@ -19,22 +18,26 @@ class CLIPResNet(nn.Module):
             for param in self.parameters():
                 param.requires_grad = False
 
+        if unfreeze_last_layer:
+            for param in self.visual.layer4.parameters():
+                param.requires_grad = True
+
     def stem_no_pool(self, x: torch.Tensor) -> torch.Tensor:
         x = self.visual.relu1(self.visual.bn1(self.visual.conv1(x)))  # type:ignore
         x = self.visual.relu2(self.visual.bn2(self.visual.conv2(x)))  # type:ignore
         x = self.visual.relu3(self.visual.bn3(self.visual.conv3(x)))  # type:ignore
         return x
 
-    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
         x = x.type(self.visual.conv1.weight.dtype)  # X shape: [B, 3, H, W]
 
         shallow = self.stem_no_pool(x)  # X shape: [B, 64, H/4, W/4]
 
-        # 2. High Level Features
-        x = self.visual.avgpool(shallow)
-        x = self.visual.layer1(x)
-        x = self.visual.layer2(x)
-        x = self.visual.layer3(x)
-        high = self.visual.layer4(x)  # X shape: [B, 2048, H/32, W/32]
+        # 2. Mid level features
+        x = self.visual.avgpool(shallow) # [B, 64, H/32, W/32]
+        x = self.visual.layer1(x)  # [B, 256, H/32, W/32]
+        middle = self.visual.layer2(x) # [B, 512, H/32, W/32]
+        x = self.visual.layer3(middle)   # [B, 1024, H/32, W/32]
+        high = self.visual.layer4(x)  # [B, 2048, H/32, W/32]
 
-        return shallow, high
+        return shallow, middle, high
