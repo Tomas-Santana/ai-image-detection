40cadc71af18aef78eae307094bae08b079cd167
Tomas Santana <tomas.30604530@uru.edu>
2026-03-23 15:42:59 -0400

    feat: remove unused code

--- util.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def mkdirs(paths):
-    if isinstance(paths, list) and not isinstance(paths, str):
-        for path in paths:
-            os.makedirs(path, exist_ok=True)
-    else:
-        os.makedirs(paths, exist_ok=True)
